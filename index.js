c1bf2e0a02aa7d2456ec43ca5101ea152d52e510
Got-crypto <pecpalmplay@gmail.com>
2023-07-20 05:18:12 +0200

    added a post request to /api/users to create a user

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
+
+app.use(cors())
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: !1}))
 
 const DB_URL = process.env.DATABASE_URL
 
@@ -42,13 +48,17 @@ const Exercise = model('Exercise', exerciseSchema)
 const User = model('User', userSchema)
 const Log = model('Log', logSchema)
 
-app.use(cors())
-app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', async (req, res) => {
+  const username = req.body
 
+  const doc = await User.create(username)
+
+  return res.json({username: doc?.username, _id: doc?._id?.toString()})
+})
 
 
 
